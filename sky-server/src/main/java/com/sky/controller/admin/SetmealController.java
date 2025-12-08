13f2a76e383a0c7a8ff2bf925f64b2a6ccc0df3f
mikubob <2386782347@qq.com>
2025-12-08 13:12:25 +0800

    feat(setmeal): 实现套餐分页查询功能
    - 在SetmealController中添加分页查询接口
    - 完善SetmealMapper接口及XML映射文件以支持分页查询
    - 更新service层接口和实现类，集成PageHelper进行分页处理
    - 优化变量命名一致性，如setMealService改为setmealService
    - 引入必要的分页相关依赖和数据传输对象

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -1,16 +1,15 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.setmealservice;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -20,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SetmealController {
 
     @Autowired
-    private setmealservice setMealService;
+    private setmealservice setmealService;
 
     /**
      * 新增套餐
@@ -31,8 +30,21 @@ public class SetmealController {
     @Operation(summary = "新增套餐")
     public Result save(@RequestBody SetmealDTO setmealDTO){
         log.info("新增套餐：{}",setmealDTO);
-        setMealService.saveWithDish(setmealDTO);
+        setmealService.saveWithDish(setmealDTO);
         return Result.success();
+    }
+
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @Operation(summary = "分页查询")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO){
+        log.info("分页查询：{}",setmealPageQueryDTO);
+        PageResult pageResult = setmealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(pageResult);
     }
 
 }
