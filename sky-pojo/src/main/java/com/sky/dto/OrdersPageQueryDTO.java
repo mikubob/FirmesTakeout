c766246df0ac530303ddb63e9844c39695bc0ff7
mikubob <2386782347@qq.com>
2025-12-17 21:15:05 +0800

    feat(order): 添加订单查询时间范围支持
    - 在 OrdersPageQueryDTO 中添加 beginTime 和 endTime 字段
    - 使用 @DateTimeFormat 注解指定时间格式为 yyyy-MM-dd HH:mm:ss
    - 引入 spring-context 依赖以支持 @DateTimeFormat 注解功能
    - 为时间字段启用 Spring Web 格式化支持

--- sky-pojo/src/main/java/com/sky/dto/OrdersPageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/OrdersPageQueryDTO.java
@@ -1,6 +1,7 @@
 package com.sky.dto;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -18,8 +19,10 @@ public class OrdersPageQueryDTO implements Serializable {
 
     private Integer status;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime beginTime;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endTime;
 
     private Long userId;
